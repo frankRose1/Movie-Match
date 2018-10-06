0b0d02f95ee562c3d006971557205fa91910265f
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-10-05 23:58:09 -0400

    Add react router and some basic configuration

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,19 @@
 import React, { Component } from 'react';
-import CreateCafe from './containers/CreateCafe';
+import {Route, Switch} from 'react-router-dom';
 import Layout from './components/Layout';
+import CreateCafe from './containers/CreateCafe';
+import Auth from './containers/Auth';
 
 class App extends Component {
-  state = {
-    data: null
-  }
-
-  // componentDidMount(){
-  //   this.call_API("/api/hello")
-  //     .then(res => {
-  //       this.setState({data: res});
-  //       console.log(this.state.data);
-  //     })
-  //     .catch(err => console.error(err))
-  // }
-
-  call_API = async (route) => {
-    const response = await fetch(route);
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message)
-    }
-
-    return body;
-  };
-
+  
   render() {
-
     return (
       <div className="App">
       <Layout>
-        <CreateCafe />
+        <Switch>
+          <Route path="/add" Component={CreateCafe} />
+          <Route path="/login" Component={Auth}/>
+        </Switch>
       </Layout>
       </div>
     );
